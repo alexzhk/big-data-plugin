81b98baa1dc5a9ad2290b43b0b226787724d2403
Vasilina Terehova <vasilina_terehova@epam.com>
2017-10-19 17:18:01 +0300

    [BAD-1050]-big-data-plugin adopt for shims on karaf

--- impl/shim/mapreduce/src/test/java/org/pentaho/big/data/impl/shim/mapreduce/MapReduceServiceImplTest.java
+++ impl/shim/mapreduce/src/test/java/org/pentaho/big/data/impl/shim/mapreduce/MapReduceServiceImplTest.java
@@ -83,9 +83,9 @@ public class MapReduceServiceImplTest {
     when( hadoopConfiguration.getHadoopShim() ).thenReturn( hadoopShim );
     resolvedJarUrl = new URL( "http://jar.net/jar" );
     List<TransformationVisitorService> visitorServices = new ArrayList<>();
-    mapReduceService =
-      new MapReduceServiceImpl( namedCluster, hadoopConfiguration, executorService, jarUtility, pluginPropertiesUtil,
-        pluginRegistry, visitorServices );
+//    mapReduceService =
+//      new MapReduceServiceImpl( namedCluster, hadoopConfiguration, executorService, jarUtility, pluginPropertiesUtil,
+//        pluginRegistry, visitorServices );
   }
 
   @Test( expected = MapReduceExecutionException.class )
